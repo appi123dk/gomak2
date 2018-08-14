b03d8ca5151c62791d280eea8a9ceec2f37f5544
appi123dk <whdidtnr224@naver.com>
2018-08-14 12:08:52 +0900

    update
    -

--- vendor/assets/javascripts/gomakproduction/gmap.js
+++ vendor/assets/javascripts/gomakproduction/gmap.js
@@ -16,16 +16,16 @@ $(document).ready(function(){
     scrollwheel: false,
 
     // The latitude and longitude to center the map (always required)
-    center: new google.maps.LatLng(37.495420, 127.036007), // Chicago
+    center: new google.maps.LatLng(37.495420, 127.036007), 
 
     // Styling of the map in JSON object
     styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cdd2d4"},{"visibility":"on"}]}]
     };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
+    var map = new google.maps.Map(document.getElementById('map'),
       mapOptions);
 
     // Map marker 
-    var image = '/assets/gomakculture/map-pin.png';
+    // var image = '/assets/gomakculture/map-pin.png';
     var myLatLng = new google.maps.LatLng(37.495420, 127.036007);
     var beachMarker = new google.maps.Marker({
       position: myLatLng,
